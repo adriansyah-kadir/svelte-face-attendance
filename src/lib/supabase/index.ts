864291c205e98bfe0543ccf9fa128fb0c6aa6474
adriansyah-kadir <adriansyahkadir293@gmail.com>
2025-03-09 04:10:19 +0800

    fix deploy github actions

--- src/lib/supabase/index.ts
+++ src/lib/supabase/index.ts
@@ -2,10 +2,10 @@ import {
   PUBLIC_SUPABASE_ANON_KEY,
   PUBLIC_SUPABASE_URL,
 } from "$env/static/public";
-import sb from "@supabase/supabase-js";
+import { createBrowserClient } from "@supabase/ssr";
 import type { Database } from "./types";
 
-export default new sb.SupabaseClient<Database>(
+export default createBrowserClient<Database>(
   PUBLIC_SUPABASE_URL,
   PUBLIC_SUPABASE_ANON_KEY
 );
