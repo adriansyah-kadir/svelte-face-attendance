efdcf8027f86ee54e2e66b694708b979c3220559
adriansyah-kadir <adriansyahkadir293@gmail.com>
2025-03-09 03:56:23 +0800

    remove supabase/ssr

--- src/lib/supabase/index.ts
+++ src/lib/supabase/index.ts
@@ -1,6 +1,11 @@
-import { PUBLIC_SUPABASE_ANON_KEY, PUBLIC_SUPABASE_URL } from "$env/static/public";
-import { createBrowserClient } from "@supabase/ssr";
+import {
+  PUBLIC_SUPABASE_ANON_KEY,
+  PUBLIC_SUPABASE_URL,
+} from "$env/static/public";
+import { SupabaseClient } from "@supabase/supabase-js";
 import type { Database } from "./types";
 
-const supabase = createBrowserClient<Database>(PUBLIC_SUPABASE_URL, PUBLIC_SUPABASE_ANON_KEY)
-export default supabase
+export default new SupabaseClient<Database>(
+  PUBLIC_SUPABASE_URL,
+  PUBLIC_SUPABASE_ANON_KEY
+);
